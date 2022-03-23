b1bf0a7e758d07f2f3800e9be14a60cbe5fead51
Lotfi-Hadjsadok <83740857+Lotfi-Hadjsadok@users.noreply.github.com>
2022-03-23 20:30:26 +0100

    init

---
+++ routes/Router.php
@@ -0,0 +1,12 @@
+<?php 
+namespace Router;
+class Router{
+    public $url;
+    public function __construct($url)
+    {
+        $this->url = $url;
+    }
+    public function show(){
+        echo $this->url;
+    }
+}
